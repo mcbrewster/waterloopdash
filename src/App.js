72eb4b0020c4e8202063689441aeb3213befea16
brewsterbabe <mcbrewst@edu.uwaterloo.ca>
2018-05-12 14:22:39 -0400

    add graph

--- src/App.js
+++ src/App.js
@@ -1,19 +1,60 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import * as V from 'victory';
+import { VictoryLine, VictoryTheme, VictoryChart,VictoryStack } from 'victory';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <div className='graph'>
+      <VictoryChart
+      theme={VictoryTheme.material}
+    >
+    <VictoryStack
+  colorScale={["tomato", "orange", "gold"]}
+>
+      <VictoryLine
+        style={{
+          data: { stroke: "#c43a31", width: "50%", height: "50%" },
+          parent: { border: "1px solid #ccc"}
+        }}
+        data={[
+          { x: 1, y: 2 },
+          { x: 2, y: 3 },
+          { x: 3, y: 5 },
+          { x: 4, y: 4 },
+          { x: 5, y: 7 }
+        ]}
+      /><VictoryLine
+      style={{
+        data: { stroke: "#c43a31", width: "50%", height: "50%" },
+        parent: { border: "1px solid #ccc"}
+      }}
+      data={[
+        { x: 1, y: 2 },
+        { x: 2, y: 3 },
+        { x: 3, y: 5 },
+        { x: 4, y: 4 },
+        { x: 5, y: 7 }
+      ]}
+    /><VictoryLine
+    style={{
+      data: { stroke: "#c43a31", width: "50%", height: "50%" },
+      parent: { border: "1px solid #ccc"}
+    }}
+    data={[
+      { x: 1, y: 2 },
+      { x: 2, y: 3 },
+      { x: 3, y: 5 },
+      { x: 4, y: 4 },
+      { x: 5, y: 7 }
+    ]}
+  /></VictoryStack>
+    </VictoryChart>
+    </div>
+  </div>
     );
   }
 }
